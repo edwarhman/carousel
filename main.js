e35bfeed3f94cd99e1a6ef3b77076e0df82c24e0
edwarhman <edwarhman@gmail.com>
2021-11-19 14:57:41 -0400

    TASK-6 add functionability to UI and fix carousel translation timing, now elements translate proper

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let carouselItems = Array.from(document.querySelectorAll(".carousel__item"));
+let carouselIndicators = Array.from(document.querySelector('.carousel__indicators').children);
+let nextButton = document.querySelector('.carousel__button.right');
+let prevButton = document.querySelector('.carousel__button.left');
 let currentActive = 0;
 let timeOutAnimation = 2000;
 
@@ -12,14 +15,27 @@ function spinCarousel(activeIndex, nextIndex, direction) {
   }
 
   next.classList.add("next", direction);
-  active.classList.add(direction);
     setTimeout(()=> {
       next.classList.add("active");
+      active.classList.add(direction);
       next.classList.remove(direction, "next");
-  }, 10);
+      carouselIndicators[activeIndex].classList.remove('active');
+      carouselIndicators[nextIndex].classList.add('active');
+    }, 10);
   setTimeout(()=> active.classList.remove("active", direction), timeOutAnimation);
 
   currentActive = nextIndex;
 }
 
-document.addEventListener('click', ()=> spinCarousel(currentActive, currentActive + 1, "right"));
+
+document.addEventListener('click', (e)=> {
+  switch(e.target) {
+    case prevButton:
+      spinCarousel(currentActive, (currentActive - 1 < 0)? carouselItems.length - 1 : currentActive - 1, "right");
+      break;
+    case nextButton:
+      spinCarousel(currentActive, (currentActive + 1>= carouselItems.length)? 0 : currentActive + 1, "left");
+      break;
+    default:
+  }
+});
